5e6be455106e560fa7039560849fe81919a4acb3
Farhan Wily <92068445+wilywho@users.noreply.github.com>
2024-07-02 14:05:11 +0700

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,6 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-import seaborn as sns
 import tensorflow as tf
 import plotly.express as px
 from tensorflow.keras.models import Sequential
